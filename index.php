08f67f62f98f40893de97fb8b7be1dc6046607ea
Kevaunjh <kevaunjh@gmail.com>
2023-11-15 00:08:27 -0500

    Various Query Views

--- index.php
+++ index.php
@@ -1,3 +1,91 @@
+<?php
+
+// connect to the database
+$conn = mysqli_connect('localhost:3306', 'root', '123456', 'RailwaySystemWebsite');
+
+// check connection
+if (!$conn) {
+    echo 'Connection error: ' . mysqli_connect_error();
+}
+
+//View 1
+
+$RI = 'SELECT Routes.RouteID, Routes.Duration, Routes.Distance, Trains.TrainID, s1.Location AS StartStation, s1.Location AS EndStation FROM Routes JOIN Trains ON Routes.OperatingTrains = Trains.TrainID JOIN Stations s1 ON Routes.StartStation = s1.StationID JOIN Stations s2 ON Routes.EndStation = s2.StationID';
+$RI2 = mysqli_query($conn, $RI);
+$RouteInfo = mysqli_fetch_all($RI2, MYSQLI_ASSOC);
+
+
+//View 2
+$PassTrains = 'SELECT T.TrainID, MAX(R.Weight) AS MaximumWeight, SUM(R.PassengerCapacity) AS TotalPassengerCount, RT.RouteID, S1.Location AS StartStation, S2.Location AS EndStation 
+                FROM Trains T 
+                JOIN RailCars R ON T.BodyRailCarType = R.ModelNumber 
+                JOIN Routes RT ON T.TrainID = RT.OperatingTrains 
+                JOIN Stations S1 ON RT.StartStation = S1.StationID 
+                JOIN Stations S2 ON RT.EndStation = S2.StationID 
+                WHERE R.Category = "Passenger" 
+                GROUP BY T.TrainID, RT.RouteID, S1.Location, S2.Location';
+
+$PasseTrain = mysqli_query($conn, $PassTrains);
+$PassengerTrains = mysqli_fetch_all($PasseTrain, MYSQLI_ASSOC);
+
+//View 3
+
+$RTC = 'SELECT r.RouteID, (SELECT COUNT(*) FROM Trains T JOIN Routes rt ON rt.OperatingTrains = t.TrainID WHERE rt.RouteID = r.RouteID) AS NumberOfTrains FROM Routes r';
+$RTC2 = mysqli_query($conn, $RTC);
+$RouteTrainCount = mysqli_fetch_all($RTC2, MYSQLI_ASSOC);
+
+//View 4
+
+$RCI = 'SELECT RailCars.ModelNumber, RailCars.ModelName FROM RailCars LEFT JOIN Trains ON RailCars.ModelNumber = Trains.EngineRailCarType UNION SELECT RailCars.ModelNumber, RailCars.ModelName FROM RailCars RIGHT JOIN Trains ON RailCars.ModelNumber = Trains.EngineRailCarType';
+$RCI2 = mysqli_query($conn, $RCI);
+$RailCarInfo = mysqli_fetch_all($RCI2, MYSQLI_ASSOC);
+
+//View 5
+
+$RS = 'SELECT * FROM (SELECT "Weekday" AS DayCategory, r.RouteID, r.StartStation, r.EndStation, rt.StartTime, rt.EndTime, rt.DayOfWeek FROM Routes r INNER JOIN RunningTimes rt ON r.RouteID = rt.RouteID WHERE rt.DayOfWeek IN ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday") UNION SELECT "Weekend" AS DayCategory, r.RouteID, r.StartStation, r.EndStation, rt.StartTime, rt.EndTime, rt.DayOfWeek FROM Routes r INNER JOIN RunningTimes rt ON r.RouteID = rt.RouteID Where rt.DayOfWeek IN ("Saturday", "Sunday")) AS CombinedData';
+$RS2 = mysqli_query($conn, $RS);
+$RailCarInfo = mysqli_fetch_all($RS2, MYSQLI_ASSOC);
+
+//View 6
+
+$Long = 'SELECT RouteID, Distance
+        FROM Routes
+        ORDER BY Distance DESC';
+$LongR = mysqli_query($conn, $Long);
+$LongestRoutes = mysqli_fetch_all($LongR, MYSQLI_ASSOC);
+
+
+//View 7
+
+$Dest = 'SELECT DISTINCT U.Username, U.Location AS CurrentLocation, S.Location AS PossibleDestination FROM User U JOIN Routes R ON U.Location = (SELECT S.Location FROM Stations S WHERE S.StationID = R.StartStation) JOIN Stations S ON R.EndStation = S.StationID';
+$DestRoute = mysqli_query($conn, $Dest);
+$PossibleDestination = mysqli_fetch_all($DestRoute, MYSQLI_ASSOC);
+
+//View 8
+$TRange = 'SELECT Routes.RouteID, RunningTimes.StartTime, Routes.StartStation FROM Routes JOIN RunningTimes ON Routes.RouteID = RunningTimes.RouteID WHERE RunningTimes.StartTime BETWEEN "09:00:00" AND "13:00:00"';
+$TimeRange = mysqli_query($conn, $TRange);
+$RouteStartBetweenTimeRange = mysqli_fetch_all($TimeRange, MYSQLI_ASSOC); 
+
+//View 9
+$WeekdayRoute = 'SELECT * 
+                FROM RunningTimes 
+                WHERE NOT (Dayofweek = "Sunday" OR Dayofweek = "Saturday")'; 
+$WdayRoute = mysqli_query($conn, $WeekdayRoute);
+$WeekdayRoutes = mysqli_fetch_all($WdayRoute, MYSQLI_ASSOC);
+
+//View 10
+$Tprices = 'SELECT AgeType, Cost 
+            FROM Ticket';
+$Ticprices = mysqli_query($conn, $Tprices);
+$TicketPrices = mysqli_fetch_all($Ticprices, MYSQLI_ASSOC);  
+
+// remove the queried items from memory
+
+// close connection
+mysqli_close($conn);
+
+?>
+
 
 
 
@@ -57,9 +145,54 @@
     <div class="tab-content">
       <div class="tab-pane fade show active" id="buy-tickets">
         <h2>Buy Tickets Content</h2>
+
+        <table>
+    <thead>
+        <tr>
+            <th>Age Type</th>
+            <th>Cost</th>
+            <!-- Add more table headers if there are additional columns -->
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($TicketPrices as $TicketPrices): ?>
+            <tr>
+                <td><?php echo $TicketPrices['AgeType']; ?></td>
+                <td><?php echo $TicketPrices['Cost']; ?></td>
+                <!-- Display additional columns accordingly -->
+            </tr>
+        <?php endforeach; ?>
+    </tbody>
+</table>
       </div>
       <div class="tab-pane fade" id="routes-schedules">
         <h2>Routes & Schedules Content</h2>
+
+        <table>
+    <thead>
+        <tr>
+            <th>RunningTimeID</th>
+            <th>RouteID</th>
+            <th>StartTime</th>
+            <th>EndTime</th>
+            <th>Day of Week</th>
+            <!-- Add more table headers if there are additional columns -->
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($WeekdayRoutes as $WeekdayRoutes): ?>
+            <tr>
+                <td><?php echo $WeekdayRoutes['RunningTimeID']; ?></td>
+                <td><?php echo $WeekdayRoutes['RouteID']; ?></td>
+                <td><?php echo $WeekdayRoutes['StartTime']; ?></td>
+                <td><?php echo $WeekdayRoutes['EndTime']; ?></td>
+                <td><?php echo $WeekdayRoutes['DayOfWeek']; ?></td>
+                <!-- Display additional columns accordingly -->
+            </tr>
+        <?php endforeach; ?>
+    </tbody>
+</table>
+
       </div>
       <div class="tab-pane fade" id="trains">
         <h2>Trains Content</h2>
@@ -78,5 +211,7 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 </body>
+
+
 
 </html>
