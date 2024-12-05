000e2b7f7c4d85621306ce85704b94b6f3873707
FahadH31 <129327022+FahadH31@users.noreply.github.com>
2024-12-04 20:32:50 -0500

    Updated Logo, Added Favicon

--- public/index.php
+++ public/index.php
@@ -4,7 +4,8 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Railway System</title>
+  <title>Railway Information System</title>
+  <link rel="icon" type="image/x-icon" href="Images/train-favicon.png">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
   <link type="text/css" rel="stylesheet" href="index-stylesheet.css">
 </head>
